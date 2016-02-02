283b5b07af954c2d0a9be0ff19e13a62bee076ac
Rémy Fèvre <r.fevre184@gmail.com>
2016-02-02 17:46:51 +0100

    Debut du client

--- sources/fr/lille1/iut/pierrefeuillereseau/Client.java
+++ sources/fr/lille1/iut/pierrefeuillereseau/Client.java
@@ -5,14 +5,16 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.*;
 
 public class Client {
 	private final static int _dgLength = 50;
 	private DatagramSocket dgSocket;
 	private DatagramPacket dgPacket;
+	private static Client client;
 
-
-	public ClientUDP() throws IOException {
+	public Client() throws IOException {
+		client = new Client();
 		dgSocket = new DatagramSocket();
 	}
 
@@ -21,7 +23,6 @@ public class Client {
 		dgPacket = new DatagramPacket(buffer, _dgLength);
 		dgSocket.receive(dgPacket);
 		return new String(dgPacket.getData(), dgPacket.getOffset(), dgPacket.getLength());
-
 	}
 
 	private void send(String msg, InetAddress address, int port) throws IOException {
@@ -33,10 +34,58 @@ public class Client {
 	}
 
 	public static void main(String[] args) throws IOException {
-		ClientUDP client = new ClientUDP();
-		String msg = "Hello ";
-		client.send(msg, InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
-		System.out.println(client.receive());
+		Scanner	sc = new Scanner(System.in);
+		char carac;
+		String reponse;
+
+		do {
+			System.out.println("================================================================================");
+			System.out.println("Bonjour, et bienvenue sur le Pierre Feuille Reseau !");
+			System.out.println("1 => Créer une partie");
+			System.out.println("2 => Rejoindre une partie");
+			System.out.println("q => Quitter");
+			System.out.println("================================================================================");
+			reponse = sc.nextLine();
+			carac = reponse.charAt(0);
+			if (carac == '1') {
+				creerPartie();
+			}
+			else if (carac == '2') {
+				rejoindrePartie();
+			}
+			else if (carac == 'q') {
+				System.out.println("Quittage en cours ...");
+			}
+		} while (carac != 'q');
+		// String msg = "Hello ";
+		// client.send(msg, InetAddress.getByName("localhost"), Integer.parseInt("3630"));
+		// System.out.println(client.receive());
+	}
+
+	private static void creerPartie() throws IOException {
+		Scanner	sc = new Scanner(System.in);
+		String pseudo, idPartie;
+		int nbJoueurs, nbManches;
+
+		System.out.println("Choisissez un pseudonyme ?");
+		pseudo = sc.nextLine();
+		System.out.println("Choisissez un identifiant pour la partie ?");
+		idPartie = sc.nextLine();
+		System.out.println("Choisissez un nombre de manches pour la partie ?");
+		sc.nextLine();
+		nbManches = sc.nextInt();
+
+		// `CREATE:PSEUDO:ID_PARTIE:NB_JOUEURS:NB_MANCHES`
+		String requete = "CREATE:"+pseudo+":"+idPartie+":2:"+nbManches;
+		client.send(requete, InetAddress.getByName("localhost"), 3630);
+	 	System.out.println(client.receive());
+	}
+
+	private static void rejoindrePartie() throws IOException {
+		Scanner	sc = new Scanner(System.in);
+
+		System.out.println("C'est oui !");
+		String str = sc.nextLine();
 	}
 
 }
